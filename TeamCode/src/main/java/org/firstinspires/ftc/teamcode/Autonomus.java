bce0a2dcc726f46c3757ee35081f627d787c0ed0
asdfghjkl403622 <samycode10@gmail.com>
2019-10-01 16:56:38 -0500

    Fixed gradle, added moveInches function to autonomus.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomus.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomus.java
@@ -2,16 +2,78 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 @Autonomous(name = "Autonomus Tank", group = "Pushbot")
 public class Autonomus extends LinearOpMode {
 
-    static final double COUNTS_PER_INCH = 42.780848752;
+    static final double COUNTS_PER_INCH = 85.561697504;
 
     Robot robot = new Robot();
+    private ElapsedTime runtime = new ElapsedTime();
 
     @Override
     public void runOpMode() throws InterruptedException {
+
+        robot.init(hardwareMap);
+
+        // Send telemetry message to signify robot waiting;
+        telemetry.addData("Status", "Resetting Encoders");
+        telemetry.update();
+
+        robot.leftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.rightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        robot.leftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.rightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        waitForStart();
+        driveInches(0.25, 4 * Math.PI, 200);
+
+
+
+    }
+
+    public void driveInches(double speed, double Inches, double timeoutS) {
+        int newLeftTarget;
+        int newRightTarget;
+
+        // Determine new target position, and pass to motor controller
+        newLeftTarget = robot.leftDrive.getCurrentPosition() + (int) (Inches * COUNTS_PER_INCH);
+        newRightTarget = robot.rightDrive.getCurrentPosition() + (int) (Inches * COUNTS_PER_INCH);
+        robot.leftDrive.setTargetPosition(newLeftTarget);
+        robot.rightDrive.setTargetPosition(newRightTarget);
+
+        // Turn On RUN_TO_POSITION
+        robot.leftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        robot.rightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        // reset the timeout time and start motion.
+        runtime.reset();
+        robot.leftDrive.setPower(Math.abs(speed));
+        robot.rightDrive.setPower(Math.abs(speed));
+
+
+        while (opModeIsActive() &&
+                (runtime.seconds() < timeoutS) &&
+                (robot.leftDrive.isBusy() && robot.rightDrive.isBusy())) {
+
+            // Display it for the driver.
+            telemetry.addData("Path1", "Running to %7d :%7d", newLeftTarget, newRightTarget);
+            telemetry.addData("Path2", "Running at %7d :%7d",
+                    robot.leftDrive.getCurrentPosition(),
+                    robot.rightDrive.getCurrentPosition());
+            telemetry.update();
+        }
+
+        // Stop all motion;
+        robot.leftDrive.setPower(0);
+        robot.rightDrive.setPower(0);
+
+        // Turn off RUN_TO_POSITION
+        robot.leftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.rightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
 
     }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -1,7 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
@@ -10,9 +9,9 @@ public class Robot {
     public DcMotor  leftDrive   = null;
     public DcMotor  rightDrive  = null;
     //this identifies the servo motors and allows the phone to identify
-    public DcMotor leftSucc = null;
-    public DcMotor rightSucc = null;
-    public DcMotor Grab = null;
+    // public DcMotor leftSucc = null;
+    // public DcMotor rightSucc = null;
+    // public DcMotor Grab = null;
     public static final double COUNTS_PER_INCH = 42.780848752;
 
 
@@ -38,32 +37,32 @@ public class Robot {
         leftDrive  = hwMap.get(DcMotor.class, "left_drive");
         rightDrive = hwMap.get(DcMotor.class, "right_drive");
         //this renames the motors in the hardware map
-        leftSucc = hwMap.get(DcMotor.class, "left_succ");
-        rightSucc = hwMap.get(DcMotor.class, "right_succ");
+        //leftSucc = hwMap.get(DcMotor.class, "left_succ");
+        //rightSucc = hwMap.get(DcMotor.class, "right_succ");
         //Define and initialize grab motor
-        Grab = hwMap.get(DcMotor.class, "Grab");
+        //Grab = hwMap.get(DcMotor.class, "Grab");
 
         leftDrive.setDirection(DcMotor.Direction.FORWARD); // Set to REVERSE if using AndyMark motors
         rightDrive.setDirection(DcMotor.Direction.REVERSE);// Set to FORWARD if using AndyMark motors
         //this sets the input of the motors
-        leftSucc.setDirection(DcMotor.Direction.FORWARD);
-        rightSucc.setDirection(DcMotor.Direction.REVERSE);
+        //leftSucc.setDirection(DcMotor.Direction.FORWARD);
+        //rightSucc.setDirection(DcMotor.Direction.REVERSE);
         //Sets motor directions for the grab motors
-        Grab.setDirection(DcMotor.Direction.FORWARD);
+        //Grab.setDirection(DcMotor.Direction.FORWARD);
 
         // Set all motors to zero power
         leftDrive.setPower(0);
         rightDrive.setPower(0);
-        leftSucc.setPower(0);
-        rightSucc.setPower(0);
-        Grab.setPower(0);
+        //leftSucc.setPower(0);
+        //rightSucc.setPower(0);
+        //Grab.setPower(0);
 
         // Set all motors to run without encoders.
         // May want to use RUN_USING_ENCODERS if encoders are installed.
         leftDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         rightDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        leftSucc.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        rightSucc.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        //leftSucc.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        //rightSucc.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
 
         // Define and initialize ALL installed servos.
@@ -71,30 +70,25 @@ public class Robot {
     }
 
     public void intakeBlock() {
-        leftSucc.setPower(intakeSpeed);
-        rightSucc.setPower(intakeSpeed);
+        //leftSucc.setPower(intakeSpeed);
+        //rightSucc.setPower(intakeSpeed);
     }
     public void ejectBlock() {
-        leftSucc.setPower(ejectSpeed);
-        rightSucc.setPower(ejectSpeed);
+        //leftSucc.setPower(ejectSpeed);
+        //rightSucc.setPower(ejectSpeed);
     }
     public void drive(double leftSpeed, double rightSpeed) {
         leftDrive.setPower(leftSpeed);
         rightDrive.setPower(rightSpeed);
     }
-    public void driveInches(double distance) {
-        leftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        rightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        leftDrive.setPower(COUNTS_PER_INCH * distance);
-        rightDrive.setPower(COUNTS_PER_INCH * distance);
-    }
+
     public void grabControl(boolean grab, boolean release) {
-        if (grab) {
-            Grab.setPower(grabSpeedGrab);
-        }
-        if (release) {
-            Grab.setPower(grabSpeedRelease);
-        }
+//        if (grab) {
+//            Grab.setPower(grabSpeedGrab);
+//        }
+//        if (release) {
+//            Grab.setPower(grabSpeedRelease);
+//        }
     }
 
 
